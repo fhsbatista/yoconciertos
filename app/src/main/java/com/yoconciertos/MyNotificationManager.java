9bf39228ebeef5a8369690ef70a63bbbb473068a
Fernando Batista <fernandohsbatista@gmail.com>
2018-10-31 13:32:59 -0200

    App rodando, implementaçao das notificaçoes esta finalizada

--- app/src/main/java/com/yoconciertos/MyNotificationManager.java
+++ app/src/main/java/com/yoconciertos/MyNotificationManager.java
@@ -1,17 +1,20 @@
 package com.yoconciertos;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.support.v4.app.NotificationCompat;
 
 public class MyNotificationManager {
 
+    private static final int NOTIFICATION_ID = 0;
     private Context mContext;
-    private int NOTIFICATION_ID = 10;
+    private String NOTIFICATION_CHANNEL_ID = "10";
 
     public MyNotificationManager(Context context){
         mContext = context;
@@ -26,18 +29,32 @@ public class MyNotificationManager {
                 PendingIntent.FLAG_UPDATE_CURRENT
         );
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(mContext);
-        Notification notification = builder
-                .setSmallIcon(R.mipmap.ic_small_icon)
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(mContext)
+                .setSmallIcon(R.drawable.ic_freesample)
                 .setAutoCancel(true)
+                .setColor(Color.DKGRAY)
                 .setContentIntent(pendingIntent)
                 .setContentTitle(from)
                 .setContentText(message)
-                .setLargeIcon(BitmapFactory.decodeResource(mContext.getResources(), R.mipmap.ic_launcher))
-                .build();
+                .setLargeIcon(BitmapFactory.decodeResource(mContext.getResources(), R.drawable.ic_freesample));
 
-        notification.flags = Notification.FLAG_AUTO_CANCEL;
+
         NotificationManager notificationManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O)
+        {
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "NOTIFICATION_CHANNEL_NAME", importance);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.DKGRAY);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            builder.setChannelId(NOTIFICATION_CHANNEL_ID);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        Notification notification = builder.build();
+        notification.flags = Notification.FLAG_AUTO_CANCEL;
+
         notificationManager.notify(NOTIFICATION_ID, notification);
     }
 }
